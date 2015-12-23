1d4af6acf4d7c28b329b899e0ec91290388c4883
jackeir <jackeir@gmail.com>
2015-12-23 18:16:45 +0800

    Part 2

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 //Starter Project for the Reddit Clone
 var app = angular.module('reddit-clone', ['ngRoute', 'firebase']);
+app.constant('fbURL', "https://reddit-chat-ortonlin.firebaseio.com/");
+
+app.factory('Posts', function($firebaseArray, fbURL) {
+  return $firebaseArray(new Firebase(fbURL));
+});
+
 
 app.config(function($routeProvider) {
   $routeProvider.when('/', {
@@ -10,6 +16,17 @@ app.config(function($routeProvider) {
       redirectTo: '/'
     })
 });
-app.controller('MainController',function($scope,firebase){
 
+app.controller('MainController', function($scope, $firebaseArray,Posts) {
+  $scope.posts = Posts;
+  $scope.savePost = function(post) {
+    Posts.$add({
+      name: post.name,
+      description: post.description,
+      url: post.url
+    });
+    post.name = "";
+    post.description = "";
+    post.url = "";
+  }
 });
